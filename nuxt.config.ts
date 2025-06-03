942116b3ca0e2eca813da6cc6c1a99b829499090
Castelo <jpcbm@ic.ufal.br>
2025-06-03 20:34:15 -0300

    Atualiza a configuração do Nuxt para incluir suporte a Tailwind CSS e PWA, desabilitando as ferramentas de desenvolvimento. Adiciona dependências necessárias no package.json e package-lock.json.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,27 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: false },
+
+  modules: ["@nuxt/eslint", "@vite-pwa/nuxt"],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  pwa: {
+    registerType: "autoUpdate",
+    devOptions: {
+      enabled: true,
+      type: "module",
+    },
+    manifest: {
+      name: "Calculadora PWA",
+      short_name: "Calculadora",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#ffffff",
+    },
+  },
+});
