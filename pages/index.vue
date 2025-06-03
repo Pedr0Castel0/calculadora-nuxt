bc996101db8a5dda94245d6c17bb939514ff03cd
Castelo <jpcbm@ic.ufal.br>
2025-06-03 20:51:30 -0300

    Adiciona o arquivo index.vue com template básico e lógica para gerenciamento de atualização e funcionamento offline do PWA.

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>Index</div>
+</template>
+
+<script setup lang="ts">
+const { $pwa } = useNuxtApp();
+
+watchEffect(() => {
+  if ($pwa?.offlineReady) {
+    alert("O app está pronto para funcionar offline!");
+  }
+
+  if ($pwa?.needRefresh) {
+    const confirmUpdate = confirm("Nova versão disponível. Atualizar?");
+    if (confirmUpdate) {
+      $pwa.updateServiceWorker();
+    }
+  }
+});
+</script>
